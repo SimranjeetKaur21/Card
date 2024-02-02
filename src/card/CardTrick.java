50da301efe2c940e9b0224580415eb65f61c0b45
SimranjeetKaur21 <kau15448@sheridancollege.ca>
2024-02-02 18:30:46 -0500

    Initial Commit

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package card;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -20,13 +21,49 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue(generateRandomValue());
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[generateRandomNumber(0,3)]);
+            magicHand[i]=c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
         // add one luckcard hard code 2,clubs
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Pick the Card value: ");
+        int userValue = scanner.nextInt();
+        System.out.print("Pick the card suit (0-3): ");
+        int userSuit = scanner.nextInt();
+        
+        
+        Card userCard = new Card();
+        userCard.setValue(userValue);
+        userCard.setSuit(Card.SUITS[userSuit]);
+        
+        boolean isCardInMagicHand= false;
+        for (Card card: magicHand){
+            if (card.equals(userCard)){
+                isCardInMagicHand = true;
+                 break;
+                
+            }  
+        }
+             if(isCardInMagicHand){
+                    System.out.println("Congratulations! your card is in the magic hand.");
+                }else{
+                    System.out.println("Sorry, your card is not in the magic hand.");
+                }
+           
     }
+            private static int generateRandomNumber(int min, int max){
+                return (int)(Math.random() * (max-min + 1)+ min);
+            }
+            
+            private static int generateRandomValue(){
+                return generateRandomNumber(1,13);
+            }
     
 }
+
